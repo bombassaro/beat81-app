fe67445219436f700c118559cf8ecba9708e7e47
Fernando <fernando.bombassaro@gmail.com>
2019-09-15 00:13:59 +0200

    Install lodash to normalize data

--- Beat81BackEnd/index.js
+++ Beat81BackEnd/index.js
@@ -3,13 +3,37 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
 const port = 3000;
+const _ = require("lodash")
 
-const { dataEvents, dataImages, members, nextDays } = require("./data")
+const { 
+  dataEvents, 
+  dataImages, 
+  members, 
+  nextDays
+} = require("./data")
 
 const store = {
+  attendance: [{
+    event: 0,
+    name: members[2].name
+  },{
+    event: 0,
+    name: members[1].name
+  },{
+    event: 1,
+    name: members[0].name
+  }],
   days: nextDays,
   events: dataEvents,
 	members: members
+}
+
+const checkin = (event, member) => {
+  const item = {
+    event: event,
+    name: member,
+  }
+  store.attendance.push(item)
 }
 
 const newMember = (firstname) => {
@@ -20,19 +44,45 @@ const newMember = (firstname) => {
   store.members.push(item)
 }
 
+const mountEvents = () => {
+  const { attendance, events } = store
+  const result = []
+  _.map(events, (event, k) => {
+    const filtered = _.filter(attendance, { event: event.id })
+    const attendees = []
+    _.map(filtered, (each, k) => {
+      const name = { 
+        name: each.name 
+      }
+      attendees.push(name)
+    })
+    const item = {
+      ...event,
+      attendees: orderList(attendees)
+    }
+    result.push(item)
+  })
+  return result
+}
+
 const orderList = (list) => {
   return list.sort((a, b) => 
-      a.name.localeCompare(b.name))
+    a.name.localeCompare(b.name))
 }
 
 io.on("connection", socket => {
   
-  console.log("a user connected :D");
-
+  socket.on("checkin", (event, member) => {
+    checkin(event, member)
+    io.emit("events/list", {
+      days: store.days,
+      events: mountEvents(), 
+    })
+  })
   socket.on("events/list", () => {
     socket.emit("events/list", {
       days: store.days,
-      events: store.events, 
+      events: mountEvents(), 
     })
   })
   socket.on("members/list", () => {
